5a0188d0154cd069e45b300809cb8c943422126a
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-27 12:02:23 -0700

    added display users and user profile w/ related crud functions

--- crud.py
+++ crud.py
@@ -19,15 +19,25 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 def get_movie_by_id(movie_id):
-    """Returns movie ny ID"""
+    """Returns movie by ID"""
 
     return Movie.query.get(movie_id)
 
-    
+def get_user_by_id(user_id):
+    """Returns user by ID"""
+
+    return User.query.get(user_id)
+
+
 def get_movies():
     """Returns all movies in database"""
 
     return Movie.query.all()
+
+def get_users():
+    """Returns all users"""
+
+    return User.query.all()
 
 def create_rating(user, movie, score):
 
